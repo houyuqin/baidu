b5a894b02934dda5d1a2c4f73563c10ed7d8e1e1
houyuqin <1738519809@qq.com>
2020-03-17 14:17:05 +0800

    -mcssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,14 @@ module.exports = function (grunt) {
         src: './index.html',
         dest: 'dist/index.html'
       }
+    },
+    cssmin: {
+      'dist/baidu.min.css':'./baidu.css'
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['htmlmin']); 
+  grunt.registerTask('default', ['cssmin']); 
 };
